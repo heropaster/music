22b7ccd32493b97c719037baf52092cbf9493bd7
Heropaster <heropasta@mail.ru>
2023-10-21 20:01:56 +0700

    start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Здесь скоро будет Skypro.Music</h1>
+		</div>
+	);
+}
+
+export default App;
